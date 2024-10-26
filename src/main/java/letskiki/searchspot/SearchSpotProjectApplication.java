3e8f50c1cd4dbb0a82181ca4846a05a26f1e6a49
박성수 <ssp0622@diffchild.com>
2024-10-26 19:55:25 +0900

    Project Initialized

---
+++ src/main/java/letskiki/searchspot/SearchSpotProjectApplication.java
@@ -0,0 +1,13 @@
+package letskiki.searchspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchSpotProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchSpotProjectApplication.class, args);
+	}
+
+}
